2bd9d8ab6b567e2b9648bd7be46a5914668998dd
Dmitry <dima.klimov.98@inbox.ru>
2021-04-04 11:19:20 +0300

    tests v1.0

--- Task1/src/test/java/by/a1qa/WikipediaTest.java
+++ Task1/src/test/java/by/a1qa/WikipediaTest.java
@@ -12,6 +12,9 @@ public class WikipediaTest extends WebDriverSettings{
     private final static String URL_WIKIPEDIA_RU_CRIB = "https://ru.wikipedia.org/wiki/" +
             "%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F" +
             ":%D0%A8%D0%BF%D0%B0%D1%80%D0%B3%D0%B0%D0%BB%D0%BA%D0%B0";
+    private final static String URL_WIKIPEDIA_RU_LOGIN = "https://ru.wikipedia.org/w/index.php?" +
+            "title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%92%D1%85%D0%BE%D0%B4&returnto=Login";
+
     private final static String EXPECTED_TITLE_WIKIPEDIA_RU = "Добро пожаловать в Википедию,";
     private final static String EXPECTED_TITLE_WIKIPEDIA_EN = "Welcome to Wikipedia,";
     private final static String EXPECTED_URL_SAVE_AS_PDF_WIKIPEDIA = "https://ru.wikipedia.org/w/index.php?" +
@@ -20,9 +23,24 @@ public class WikipediaTest extends WebDriverSettings{
             "action=show-download-screen";
     private final static String EXPECTED_IMG_FEATURED_ARTICLE = "img";
     private final static String EXPECTED_EXAMPLE_SIMPLE_TEXT = "Редактировать просто — что введёшь, то и увидишь.";
-    private final static String EXPECTED_BUTTON_SETTING_LANGUAGE = "uls-settings-trigger";
-
-
+    private final static String EXPECTED_BUTTON_SETTING_LANGUAGE_CLASS = "uls-settings-trigger";
+    private final static String EXPECTED_LABEL_USER_NAME = "Имя учётной записи";
+    private final static String EXPECTED_FIELD_USER_NAME_CLASS = "loginText mw-ui-input";
+    private final static String EXPECTED_LABEL_PASSWORD = "Пароль";
+    private final static String EXPECTED_FIELD_PASSWORD_CLASS = "loginPassword mw-ui-input";
+    private final static String EXPECTED_FIELD_CHECKBOX_CLASS = "mw-userlogin-rememberme";
+    private final static String EXPECTED_FIELD_BUTTON_LOGIN_VALUE = "Войти";
+    private final static String EXPECTED_HREF_USER_LOGIN_HELP = "https://ru.wikipedia.org/wiki/" +
+            "%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F:" +
+            "%D0%9F%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_" +
+            "%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B5";
+    private final static String EXPECTED_HREF_PASSWORD_RESET = "https://ru.wikipedia.org/wiki/" +
+            "%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:" +
+            "%D0%A1%D0%B1%D1%80%D0%BE%D1%81_%D0%BF%D0%B0%D1%80%D0%BE%D0%BB%D1%8F";
+    private final static String EXPECTED_HREF_JOIN_THE_PROJECT = "https://ru.wikipedia.org/w/" +
+            "index.php?title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:" +
+            "%D0%A1%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C_%D1%83%D1%87%D1%91%D1%82%D0%BD%D1%83%D1%8E_" +
+            "%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C&returnto=Login&campaign=loginCTA";
 
     private final static String XPATH_WIKIPEDIA_TITLE_RU = "//div[@class='mw-parser-output']//*[@class='mw-headline']";
     private final static String XPATH_WIKIPEDIA_TITLE_EN = "//div[@class='mw-parser-output']//*[@id='mp-welcome']";
@@ -32,6 +50,16 @@ public class WikipediaTest extends WebDriverSettings{
     private final static String XPATH_WIKIPEDIA_EXAMPLE_SIMPLE_TEXT =
             "//div[@class='mw-parser-output']/table/tbody/tr[4]/td[2]/p/tt";
     private final static String XPATH_WIKIPEDIA_BUTTON_SETTING_LANGUAGE = "//button[@class='uls-settings-trigger']";
+    private final static String XPATH_WIKIPEDIA_LABEL_USER_NAME = "//label[@for='wpName1']";
+    private final static String XPATH_FIELD_USER_NAME = "//input[@id='wpName1']";
+    private final static String XPATH_WIKIPEDIA_LABEL_PASSWORD = "//label[@for='wpPassword1']";
+    private final static String XPATH_FIELD_PASSWORD = "//input[@id='wpPassword1']";
+    private final static String XPATH_FIELD_CHECKBOX_REMEMBER = "//input[@id='wpRemember' and @type='checkbox']";
+    private final static String XPATH_FIELD_BUTTON_LOGIN = "//button[@id='wpLoginAttempt']";
+    private final static String XPATH_HREF_USER_LOGIN_HELP = "//div[contains(@class, 'mw-userlogin-help')]//a";
+    private final static String XPATH_HREF_PASSWORD_RESET = "//div[@class='mw-htmlform-field-HTMLInfoField " +
+            "mw-form-related-link-container mw-ui-vform-field']//a";
+    private final static String XPATH_HREF_JOIN_THE_PROJECT = "//div[@id='mw-createaccount-cta']//a";
 
 
     @Test
@@ -79,6 +107,79 @@ public class WikipediaTest extends WebDriverSettings{
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL_WIKIPEDIA_RU_CRIB);
         String attribute = driver.findElement(By.xpath(XPATH_WIKIPEDIA_BUTTON_SETTING_LANGUAGE)).getAttribute("class");
-        Assert.assertEquals(EXPECTED_BUTTON_SETTING_LANGUAGE, attribute);
+        Assert.assertEquals(EXPECTED_BUTTON_SETTING_LANGUAGE_CLASS, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginLabelUserName() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String label = driver.findElement(By.xpath(XPATH_WIKIPEDIA_LABEL_USER_NAME)).getText();
+        Assert.assertEquals(EXPECTED_LABEL_USER_NAME, label);
+    }
+
+    @Test
+    public void getWikipediaLoginFieldUserName() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_FIELD_USER_NAME)).getAttribute("class");
+        Assert.assertEquals(EXPECTED_FIELD_USER_NAME_CLASS, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginLabelPassword() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String label = driver.findElement(By.xpath(XPATH_WIKIPEDIA_LABEL_PASSWORD)).getText();
+        Assert.assertEquals(EXPECTED_LABEL_PASSWORD, label);
+    }
+
+    @Test
+    public void getWikipediaLoginFieldPassword() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_FIELD_PASSWORD)).getAttribute("class");
+        Assert.assertEquals(EXPECTED_FIELD_PASSWORD_CLASS, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginCheckBoxRemember() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_FIELD_CHECKBOX_REMEMBER)).getAttribute("class");
+        Assert.assertEquals(EXPECTED_FIELD_CHECKBOX_CLASS, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginButtonLogin() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_FIELD_BUTTON_LOGIN)).getAttribute("value");
+        Assert.assertEquals(EXPECTED_FIELD_BUTTON_LOGIN_VALUE, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginHrefUserLoginHelp() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_HREF_USER_LOGIN_HELP)).getAttribute("href");
+        Assert.assertEquals(EXPECTED_HREF_USER_LOGIN_HELP, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginHrefPasswordReset() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_HREF_PASSWORD_RESET)).getAttribute("href");
+        Assert.assertEquals(EXPECTED_HREF_PASSWORD_RESET, attribute);
+    }
+
+    @Test
+    public void getWikipediaLoginHrefJoinTheProject() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL_WIKIPEDIA_RU_LOGIN);
+        String attribute = driver.findElement(By.xpath(XPATH_HREF_JOIN_THE_PROJECT)).getAttribute("href");
+        Assert.assertEquals(EXPECTED_HREF_JOIN_THE_PROJECT, attribute);
     }
 }
+
