514efe01d36f02a74769aca1f3c0e2b0a9b74f34
klimov <dima.klimov.98@inbox.ru>
2021-04-15 13:59:13 +0300

    basicAuth

---
+++ src/test/java/by/a1qa/klimov/tests/TheInternetHerokuAppCom.java
@@ -0,0 +1,12 @@
+package by.a1qa.klimov.tests;
+
+import lombok.extern.log4j.Log4j;
+import org.testng.annotations.Test;
+
+@Log4j
+public class TheInternetHerokuAppCom extends TestSettings {
+    @Test
+    public void basicAuth() {
+
+    }
+}
