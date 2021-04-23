349e171c1b5daa39a845323ac7f75ab322411de5
SixtyNineProg <dima.klimov.98@inbox.ru>
2021-04-23 19:13:28 +0300

    Randomizer

--- src/test/java/by/a1qa/klimov/framework/utils/Randomizer.java
+++ src/test/java/by/a1qa/klimov/framework/utils/Randomizer.java
@@ -3,10 +3,11 @@ package by.a1qa.klimov.framework.utils;
 import java.util.Random;
 
 public class Randomizer {
+    private static final int LOW_CODE_ASCII_CHAR = 65;
+    private static final int HIGH_CODE_ASCII_CHAR = 122;
+
     public static String generateRandomText(int length) {
-        Random random = new Random();
-        return random.ints(48, 122)
-                .filter(i -> (i < 57 || i > 65) && (i < 90 || i > 97))
+        return new Random().ints(LOW_CODE_ASCII_CHAR, HIGH_CODE_ASCII_CHAR)
                 .mapToObj(i -> (char) i)
                 .limit(length)
                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
