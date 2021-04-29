4b6226c9e810ed15a19ca9ab4301acb279b09f7a
klimov <dima.klimov.98@inbox.ru>
2021-04-29 15:45:55 +0300

    BaseTest.java

---
+++ src/test/java/by/a1qa/klimov/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package by.a1qa.klimov.tests;
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
+import aquality.selenium.elements.interfaces.IElementFactory;
+import by.a1qa.klimov.theinternet.TheInternetPage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+    protected final IElementFactory elementFactory;
+
+    protected BaseTest() {
+        elementFactory = AqualityServices.getElementFactory();
+    }
+
+    @BeforeMethod
+    protected void beforeMethod() {
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        if (AqualityServices.isBrowserStarted()) {
+            AqualityServices.getBrowser().quit();
+        }
+    }
+
+    protected void navigate(TheInternetPage page) {
+        getBrowser().goTo(page.getAddress());
+    }
+
+    protected Browser getBrowser() {
+        return AqualityServices.getBrowser();
+    }
+}
