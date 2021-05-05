92ebc016b9f108f04fc2d4b867d4203281744ce5
SixtyNineProg <dima.klimov.98@inbox.ru>
2021-05-05 18:08:21 +0300

    delete SelectOptions.java

--- src/test/java/by/a1qa/klimov/elements/interfaces/ISelectOptions.java
+++
@@ -1,7 +0,0 @@
-package by.a1qa.klimov.elements.interfaces;
-
-import aquality.selenium.elements.interfaces.IElement;
-
-public interface ISelectOptions extends IElement {
-    void selectByIndex(int index);
-}
