6dec52384f45bd0be3ae65e4b2305d074bbb5db7
Dmitry <dima.klimov.98@inbox.ru>
2021-06-11 23:57:45 +0300

    pom

--- src/test/java/by/a1qa/klimov/AppTest.java
+++ src/test/java/by/a1qa/klimov/AppTest.java
@@ -8,9 +8,6 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
